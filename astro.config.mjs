f499b563058e8b5e99e89284400039db26f5728e
geovanny <ageovannyp@hotmail.com>
2026-04-07 16:02:36 -0500

    UpdateV3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://arnbzk7.github.io',
   integrations: [tailwindcss()],
 });
